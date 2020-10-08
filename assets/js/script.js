3a91487e5370bfb9ccdd3bc73863871dba796fd1
tmessall <tmessall17@gmail.com>
2020-10-08 11:14:23 -0600

    Got dropdown workng

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
 $(document).ready(function () {
-    // Allows dropdown to use jquery dropdown functions
-    $("#speed").selectmenu();
-    $(".jumbotron").css("z-index", 0);
-    $(document).css("z-index", 0);
-    $("#speed").css("z-index", 10000000);
-    console.log($("#speed").css("z-index"))
-
     // Grabbing elements from html
     var subBtn = $(".subBtn");
     
-    subBtn.click(function(e) {
-        console.log($("#speed option:selected").val());
-        localStorage.setItem("park", $("#speed option:selected").val());
+    subBtn.on("click", function(e) {
+        localStorage.setItem("park", $("#parks option:selected").val());
         e.preventDefault();
         // Takes user to next page
-        location.href="parks.html";
+        // location.href="parks.html";
     });
 });
