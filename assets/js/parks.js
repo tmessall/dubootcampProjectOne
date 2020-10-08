eaabd72775bd03084d0fc6ca5ea97b3366c47096
tmessall <tmessall17@gmail.com>
2020-10-08 12:18:58 -0600

    Added button to select new park, centered title

--- assets/js/parks.js
+++ assets/js/parks.js
@@ -1,51 +1,44 @@
-//global var
-//parkCode=park code for URL query 
-var parkCode=localStorage.getItem("park");
+$(document).ready(function() {
 
-
-
-
-//guery URL and response 
+    //global var
+    //parkCode=park code for URL query 
+    var parkCode=localStorage.getItem("park");
+    
+    
+    
+    
+    //guery URL and response 
     //// Here we construct our URL
     var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkCode 
     + "&api_key=AFoUZdt3nEoCawGmRJB9OY3AojoJTqxFYJyzbAXk";
-
-     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response){
-    console.log(response);
-    console.log(response.data[0].fullName);
-    console.log(response.data[0].description);
-    console.log(response.data[0].images[0].url);
-    console.log(response.data[0].images[0].altText);
-
-    $(".parkTitle").text(response.data[0].fullName);
-    $(".info").text(response.data[0].description);
-    var imgTag = $("<img>").attr("src", response.data[0].images[0].url).attr("style", "width: 100%").addClass("mx-auto");
-    $(".park-img").append(imgTag);
-    $(".caption").text(response.data[0].images[0].altText);
-
     
-   });
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        console.log(response.data[0].fullName);
+        console.log(response.data[0].description);
+        console.log(response.data[0].images[0].url);
+        console.log(response.data[0].images[0].altText);
+        
+        $(".parkTitle").text(response.data[0].fullName);
+        $(".info").text(response.data[0].description);
+        var imgTag = $("<img>").attr("src", response.data[0].images[0].url).attr("style", "width: 100%").addClass("mx-auto");
+        $(".park-img").append(imgTag);
+        $(".caption").text(response.data[0].images[0].altText);
+        
+        
+    });
+    
+    var weatherPage = $("#weather-page");    
+    var parkPage = $("#park-page");
+    
+    weatherPage.click(function(e){
+        location.href="weather.html";
+    });
 
-   var weatherPage = $("#weather-page")
-
-weatherPage.click(function(e){
-location.href="weather.html";
+    parkPage.click(function(e){
+        location.href="index.html";
+    });
 })
-
-//local storage function 
-
-
-//replace h1 with data from object/array 
-
-//use local storage to replace placeholder text for park information.
-
-//use local storage to replace placeholder text with an image tag.
-    //create and image tag 
-
-//link weather button that will take user to the weather page. 
-
-//call local storage 
-
